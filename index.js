8854e15fcb4878de7670edc16b61b04da1fd4300
DiegoBraseroSanchez <diegobraserosanchez@alumnos.cei.es>
2023-05-25 12:31:10 +0200

    Este es mi primer Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const http = require('http')
+// const server = http.createServer();
+// server.listen(3000,function(){
+//     console.log('Estamos en los años 3000');
+// });
+
+// const colors = require('colors');
+// console.log('Diego'.yellow);
+
+const colors = require('colors');
+console.log(colors.trap("holaaa"));
